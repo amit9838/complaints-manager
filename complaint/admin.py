0511d2d385a95163182f58072a35154d6c7eee85
Amit Chaudhary <amitchaudhry0539@gmail.com>
2022-08-05 19:04:18 +0530

    Added, Manage components associated with a Complaint from django admin

--- complaint/admin.py
+++ complaint/admin.py
@@ -1,15 +1,22 @@
+from dataclasses import fields
 from django.contrib import admin
 
 from user.models import Engineer
 from .models import Complaint, Item
 # Register your models here.
-from django.contrib.admin import SimpleListFilter
 
+admin.site.site_header = "Marca Admin"
+admin.site.site_title = "Marca Admin Area"
+admin.site.index_title = "Welcome to the Marca Admin Area"
 
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 1
 
 class ComplaintAdmin(admin.ModelAdmin):
     model = Complaint
     list_display=['get_product','registred_date', 'registred_by','assigned_to', 'complaint_status']
+    inlines= [ItemInline]
 
     def get_product(self,obj):
         return obj.product_name +"-"+obj.product_model +"(" +obj.customer_name+")"
