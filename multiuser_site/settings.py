8b5b5f21729a85f2730373be8adea0cbb2313328
Amit Chaudhary <amitchaudhary0539@gmail.com>
2023-06-27 16:21:24 +0530

    add local routes

--- multiuser_site/settings.py
+++ multiuser_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.110","127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.0.110","127.0.0.1","*"]
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
